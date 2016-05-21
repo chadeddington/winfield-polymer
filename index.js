6f84f50f453e01b10b4506d48da8d8831a2feb4b
Chad Eddington <cdavideddington@gmail.com>
2016-05-21 17:58:53 -0600

    First commit

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+
+// Set the template engine
+app.set('view engine', 'ejs');
+
+// Base route
+app.get('/', function(request, response) {
+  response.render('home');
+});
+
+// Tasks route
+app.get('/tasks', function(request, response) {
+	response.render('pages/tasks');
+});
+
+// Server starts listening
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
